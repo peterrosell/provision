b264f623b5e0b315e198a17a711dd424965c770f
Greg Althaus <galthaus@austin.rr.com>
2018-08-27 14:16:14 -0500

    Add more module support

--- ipxe/build.sh
+++ ipxe/build.sh
@@ -4,6 +4,8 @@ set -e
 [ -d ipxe ] || git clone git://git.ipxe.org/ipxe.git
 # Work around a bug in building host utils in ipxe
 sed -i -e '/^HOST_CFLAGS/ s/\$(WORKAROUND_CFLAGS)//' ipxe/src/Makefile.housekeeping
+# Make sure we have enough module space for vmware.
+sed -i -e "s/MAX_MODULES 8/MAX_MODULES 100/g" ipxe/src/arch/x86/image/multiboot.c
 for t in lkrn undionly snponly_x86_64 snponly_arm64; do
     docker run -v $PWD/ipxe:/src/ipxe:rw ipxebuild /bin/build_ipxe.sh "$t"
 done
