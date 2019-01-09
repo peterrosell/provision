533af9ccbd71bbeb36dc97df4e9acaea20841685
Greg Althaus <galthaus@austin.rr.com>
2019-01-09 14:36:01 -0600

    Add a test dr-provision that can be compiled without anything else.  It will not have the embedded in it.

---
+++ cmds/dr-test/dr-test.go
@@ -0,0 +1,26 @@
+// Package main DigitalRebar Provision Server - TEST
+//
+// An RestFUL API-driven Provisioner and DHCP server
+//
+package main
+
+import (
+	"os"
+
+	"github.com/digitalrebar/provision/server"
+	"github.com/jessevdk/go-flags"
+)
+
+var c_opts server.ProgOpts
+
+func main() {
+	parser := flags.NewParser(&c_opts, flags.Default)
+	if _, err := parser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+	server.Server(&c_opts)
+}
