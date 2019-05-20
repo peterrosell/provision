af39c060e33b38b747b8ef7c56a6915466d3a4bd
Greg Althaus <galthaus@austin.rr.com>
2019-05-19 19:11:44 -0500

    Add darwin cli so that the UX can serve it

--- tools/build_cli.sh
+++ tools/build_cli.sh
@@ -3,3 +3,4 @@
 GOOS=linux GOARCH=amd64 go build -ldflags "$VERFLAGS" -o ../embedded/assets/drpcli.amd64.linux ../cmds/drpcli/drpcli.go
 GOOS=linux GOARCH=arm64 go build -ldflags "$VERFLAGS" -o ../embedded/assets/drpcli.arm64.linux ../cmds/drpcli/drpcli.go
 GOOS=windows GOARCH=amd64 go build -ldflags "$VERFLAGS" -o ../embedded/assets/drpcli.amd64.windows ../cmds/drpcli/drpcli.go
+GOOS=darwin GOARCH=amd64 go build -ldflags "$VERFLAGS" -o ../embedded/assets/drpcli.amd64.darwin ../cmds/drpcli/drpcli.go
